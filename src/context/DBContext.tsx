a3f1ae0fdc7aa6dbc3548fd1f653f383f08d45c7
LeandroTamola <leatamola@gmail.com>
2021-06-06 19:14:29 -0300

    added sql and database context

---
+++ src/context/DBContext.tsx
@@ -0,0 +1,51 @@
+import React, {createContext, useEffect, useRef, useState} from 'react';
+import {IDatabaseContract} from '../contracts/DatabaseContract';
+
+import {PodcastModel} from '../models/PodcastModel';
+import {SQliteServices} from '../services/sqliteServices';
+
+interface DBContextProps {
+  podcasts: PodcastModel[];
+  subToPodcast: (podcast: PodcastModel) => Promise<void>;
+}
+
+export const DBContext = createContext<DBContextProps>({
+  podcasts: [],
+  subToPodcast: () => Promise.resolve(),
+});
+
+export const DBProvider: React.FC = ({children}) => {
+  const [podcasts, setPodcasts] = useState<PodcastModel[]>([]);
+  const db = useRef<IDatabaseContract | null>(null);
+
+  useEffect(() => {
+    db.current = new SQliteServices();
+  }, []);
+
+  useEffect(() => {
+    if (db.current?.isReady) {
+      (async () => {
+        if (db.current) {
+          const _podcasts = await db.current.getAllPodcast();
+          setPodcasts(_podcasts);
+        }
+      })();
+    }
+  }, [db.current?.isReady]);
+
+  const subToPodcast = async (podcast: PodcastModel) => {
+    if (db.current) {
+      await db.current.subscribeToPodcast(podcast);
+      const _podcasts = await db.current.getAllPodcast();
+      setPodcasts(_podcasts);
+    }
+  };
+
+  const value: DBContextProps = {
+    podcasts,
+    subToPodcast,
+  };
+  return <DBContext.Provider value={value}>{children}</DBContext.Provider>;
+};
+
+export default DBContext;
